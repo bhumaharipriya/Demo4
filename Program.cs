8f8731f89231fefe1a68f742da2ed0c040c15bd3
bhmaharipriya <bhumaharipriya25@gmail.com>
2021-11-11 16:11:22 +0530

    new changes

--- Program.cs
+++ Program.cs
@@ -7,6 +7,14 @@ namespace Demo4
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+             Console.Write("Enter first number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int c = a + b;
+            Console.WriteLine("Addition result: " + c);
         }
     }
 }
